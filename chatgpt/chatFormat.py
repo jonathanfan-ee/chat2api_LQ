6cfec6ebcd9231c1a8fe2735a51bc421c5b97d45
jonathanfan-ee <jonathanfan-cs@outlook.com>
2024-11-04 05:43:19 +0800

    feat: 更新 chatFormat.py 中的文本格式化逻辑并将版本号更新至 1.5.11DALLEtest

--- chatgpt/chatFormat.py
+++ chatgpt/chatFormat.py
@@ -275,7 +275,8 @@ async def stream_response(service, response, model, max_tokens):
                             else:
                                 if role == 'assistant' and last_role != 'assistant':
                                     if recipient == 'dalle.text2im':
-                                        new_text = f"\n```{recipient}\n{part[len_last_content:]}"
+                                        # new_text = f"\n```{recipient}\n{part[len_last_content:]}"
+                                        new_text = f"\n\n{part[len_last_content:]}"
                                     elif last_role == None:
                                         new_text = part[len_last_content:]
                                     else:
